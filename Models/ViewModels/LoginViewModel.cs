5e049af73acbcd3da3b3a6c84c93e6872bb27cea
Florian <florian2006email@gmail.com>
2025-01-22 19:31:10 +0100

    Add all view models

---
+++ Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GimpiesBlazor.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Vul uw gebruikersnaam in")]
+        public string Username { get; set; } = string.Empty;
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Vul uw email in")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Vul uw wachtwoord in")]
+        public string Password { get; set; } = string.Empty;
+    }
+}
---
+++ Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GimpiesBlazor.Models.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Vul uw gebruikersnaam in")]
+        public string Username { get; set; } = string.Empty;
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Vul uw E-Mail in")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Vul uw wachtwoord in")]
+        public string Password { get; set; } = string.Empty;
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Herhaal uw wachtwoord")]
+        public string PasswordRepeat { get; set; } = string.Empty;
+    }
+}
