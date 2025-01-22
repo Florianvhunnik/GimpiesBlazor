683cc197eff8bb9fadaa1304590beea7bbb1c500
Florian <florian2006email@gmail.com>
2025-01-22 20:52:55 +0100

    Add AddDbContext to services, use connectionstring and use Data and EntityFrameworkCore

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using MudBlazor.Services;
 using GimpiesBlazor.Components;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using GimpiesBlazor.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -27,6 +29,12 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
 
 // Add services to the container.
 builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
