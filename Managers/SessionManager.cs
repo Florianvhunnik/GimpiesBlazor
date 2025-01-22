749aa28f1d3af89f685139d4cb0b66d70f450683
Florian <florian2006email@gmail.com>
2025-01-22 22:19:34 +0100

    Add session manager

---
+++ Managers/SessionManager.cs
@@ -0,0 +1,22 @@
+﻿using GimpiesBlazor.Models.Entities;
+
+namespace GimpiesBlazor.Managers
+{
+    public class SessionManager
+    {
+        public static bool IsLoggedIn => _account != null;
+        private static Account? _account;
+        public static string Role => _account?.Role.RoleName ?? string.Empty;
+
+        public static void SetAccount(Account account)
+        {
+            _account = account;
+        }
+
+        public static bool HasPermission(string permissionName)
+        {
+            return _account != null &&
+                   _account.Role.RolePermissions.Any(rp => rp.Permission.Name == permissionName);
+        }
+    }
+}
