0bd25c1b6f8f1e016d558cf017eff43cb4f4f23b
Florian <florian2006email@gmail.com>
2025-01-22 19:36:14 +0100

    Create Authorization.cs

---
+++ Data/Authorization.cs
@@ -0,0 +1,33 @@
+﻿namespace GimpiesBlazor.Data
+{
+    public class Authorization
+    {
+        public const string ViewStock = "ViewStock";
+        public const string AddShoe = "AddShoe";
+        public const string EditShoe = "EditShoe";
+        public const string DeleteShoe = "DeleteShoe";
+
+        public const string ViewAccounts = "ViewAccounts";
+        public const string CreateAccount = "CreateAccount";
+        public const string EditAccount = "EditAccount";
+        public const string DeleteAccount = "DeleteAccount";
+
+        public const string ViewSales = "ViewSales";
+
+        public static List<string> GetPermissions()
+        {
+            return
+            [
+                ViewStock,
+                AddShoe,
+                EditShoe,
+                DeleteShoe,
+                ViewAccounts,
+                CreateAccount,
+                EditAccount,
+                DeleteAccount,
+                ViewSales
+            ];
+        }
+    }
+}
