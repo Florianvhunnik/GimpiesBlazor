a7b18c1af6e042b671bf70ce4840f77c24b170ff
Florian <florian2006email@gmail.com>
2025-01-31 22:07:56 +0100

    Create stockmanager that we can use in our stock managing page

---
+++ Managers/StockManager.cs
@@ -0,0 +1,45 @@
+﻿using GimpiesBlazor.Data;
+using GimpiesBlazor.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GimpiesBlazor.Managers
+{
+    public class StockManager(AppDbContext context)
+    {
+        public async Task<List<Stock>> GetStock()
+        {
+            return await context.Stock
+                .Include(s => s.Brand)
+                .Include(s => s.Color)
+                .Include(s => s.ShoeType)
+                .Where(s => s.IsActive)
+                .ToListAsync();
+        }
+
+        public async Task AddStock(Stock stock)
+        {
+            context.Stock.Add(stock);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task UpdateStock(Stock stock)
+        {
+            context.Stock.Update(stock);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task DeleteStock(int stockId)
+        {
+            var stock = await context.Stock.FindAsync(stockId);
+
+            if (stock is null)
+                return;
+            if (!stock.IsActive)
+                return;
+
+            stock.IsActive = false;
+            await context.SaveChangesAsync();
+        }
+
+    }
+}
